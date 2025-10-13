c5e77ff1daa81513d4b1489770e2e7a1dca88f5c
AbdullahAlNaim <aanaim96@gmail.com>
2025-10-13 17:11:52 -0400

    binary search tree all methods

--- binary-search-tree/bst1.py
+++ binary-search-tree/bst1.py
@@ -28,3 +28,61 @@ class BinarySearchTree:
                 current_node = current_node.right
         return self
     
+    def contains(self, value):
+        current_node = self.root
+        while current_node:
+            if value == current_node.value:
+                return True
+            if value < current_node.value:
+                current_node = current_node.left
+            else:
+                current_node = current_node.right
+        return False
+    
+    def remove(self, value, start=None, parent=None):
+        current = start or self.root
+        while current and current.value != value:
+            parent = current
+            if value < current.value:
+                current = parent.left
+            else:
+                current = parent.right
+        if not current:
+            raise Exception("item not in tree")
+        if not current.right and not current.left:
+            return self._remove_node_no_children(current, parent)
+        if current.right and current.left:
+            return self._remove_node_two_children(current)
+        return self._remove_node_one_child(current, parent)
+    
+    def _remove_node_no_children(self, current, parent):
+        if current is self.root:
+            self.root = None
+            return self
+        if parent.left == current:
+            parent.left = None
+        else:
+            parent.right = None
+        return self
+    
+    def _remove_node_one_child(self, current, parent):
+        if current is self.root:
+            self.root = current.right if current.right else current.left
+            return self
+        if parent.right == current:
+            parent.right = current.right if current.right else current.left
+        else:
+            parent.left = current.right if current.right else current.left
+        return self
+    
+    def _remove_node_two_children(self, current):
+        successor = self._get_successor(current)
+        current.value = successor.value
+        return self.remove(successor.value, start=current.right, parent = current)
+
+    @staticmethod
+    def _get_successor(current):
+        successor = current.right
+        while successor and successor.left:
+            successor = successor.left
+        return successor
