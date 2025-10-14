dd2a209760b095e7cb23456930f1fff9e09648aa
AbdullahAlNaim <aanaim96@gmail.com>
2025-10-14 17:26:45 -0400

    tree traveral breadth for search

---
+++ Trees/tree1.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def breadth_first_traveral(self):
+        if not self.root:
+            raise Exception("Tree is empty")
+        queue = deque()
+        queue.append(self.root)
+        visited = []
+        while queue:
+            visited_node = queue.popleft()
+            visited.append(visited_node.data)
+            if visited_node.left:
+                queue.append(visited_node.left)
+            if visited_node.right:
+                queue.append(visited_node.right)
+        return visited
+
