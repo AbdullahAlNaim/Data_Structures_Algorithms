08e049a246d480300f0b021c01f26cf9c25f2c30
AbdullahAlNaim <aanaim96@gmail.com>
2025-10-28 17:02:35 -0400

    bst insert

---
+++ Trees/tree2.py
@@ -0,0 +1,31 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    
+    def insert(self, value):
+        new_node = Node(value)
+
+        if not self.root:
+            self.root = new_node
+            return self
+        current_node = self.root
+        while value != current_node.value:
+            if value < current_node.value:
+                if not current_node.left:
+                    current_node.left = new_node
+                    break
+                current_node = current_node.left
+            else:
+                if not current_node.right:
+                    current_node.right = new_node
+                    break
+                current_node = current_node.right
+        return self
